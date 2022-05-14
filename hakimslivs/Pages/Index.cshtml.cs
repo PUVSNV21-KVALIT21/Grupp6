716c217b7f7717ec60a5fa443ecdb68a5ecd40c6
mikaelaolsson <mikaelajolsson@hotmail.com>
2022-05-14 14:34:51 +0200

    Search function

--- hakimslivs/Pages/Index.cshtml.cs
+++ hakimslivs/Pages/Index.cshtml.cs
@@ -22,18 +22,25 @@ namespace hakimslivs.Pages
         public IList<Item> Items { get;set; }
         public IList<Category> Categories { get;set; }
         public string CurrentCategory { get; set; }
+        [FromQuery]
+        public string SearchTerm { get; set; }
 
         public async Task OnGetAsync(string currentCategory)
         {
+            IQueryable<Item> query = _context.Items.Include(i => i.Category);
+            Categories = await _context.Categories.ToListAsync();
+            if (!String.IsNullOrEmpty(SearchTerm))
+            {
+                query = query.Where(p => p.Product.ToLower().Contains(SearchTerm.ToLower()) || p.Category.Name.ToLower().Contains(SearchTerm.ToLower()) || p.Description.ToLower().Contains(SearchTerm.ToLower()) || p.Price.ToString().Contains(SearchTerm) || p.Stock.ToString().Contains(SearchTerm));
+            }
+
+
             if (!String.IsNullOrEmpty(currentCategory))
             {
-                Items = await _context.Items.Include(i => i.Category).Where(i => i.Category.Name == currentCategory).ToListAsync();
+                query = query.Where(i => i.Category.Name == currentCategory);
             }
-            else
-            {
-                Items = await _context.Items.Include(i => i.Category).ToListAsync();
-            }
-            Categories = await _context.Categories.ToListAsync();
+
+            Items = await query.ToListAsync();
         }
     }
 }
